0e4adb57a4032e89ea794ac8ca3d8b7cd22bef34
prateekaryanmodi <prateekaryanmodi@gmail.com>
2026-04-11 20:33:57 +0530

    basic funstions of strings

---
+++ basic string functions.py
@@ -0,0 +1,6 @@
+str1= "i am learning basic functions of string in python"
+print(str1.endswith("on"))
+print(str1.capitalize())
+print(str1.count("n"))
+print(str1.find("basic"))
+print(str1.replace("basic","testing replace function"))
