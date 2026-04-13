54140f4ac2f6c22c70554d107de57436f7ada12b
prateekaryanmodi <prateekaryanmodi@gmail.com>
2026-04-13 14:38:27 +0530

    Update tarffic lights using if and elif statements.py

--- tarffic lights using if and elif statements.py
+++ tarffic lights using if and elif statements.py
@@ -1,12 +1,12 @@
 #hi
 #conditional statements
 light = input("what is the color of the traffic light? ")
-if light == "not working":
-    print("proceed with caution")
 if light == "red":
     print("stop")
 elif light == "yellow":
     print("start your engine ")
 elif light == "green":
     print("go")
+else :
+    print("traffic lights not working")   
        
