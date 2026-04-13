e847fbfa4418948d5b5ced4b7cfc5afe69e2295a
prateekaryanmodi <prateekaryanmodi@gmail.com>
2026-04-13 15:47:01 +0530

    WAP to find whether a number entered is a multiple of 7 or not

---
+++ #WAP to find whether a number entered is a multiple of 7 or not .py
@@ -0,0 +1,6 @@
+#WAP to find whether a number entered is a multiple of 7 or not
+num1= int(input("Enter a number: "))
+if num1 % 7 == 0:
+    print("The number is a multiple of 7.")
+else:
+    print("The number is not a multiple of 7.") 
