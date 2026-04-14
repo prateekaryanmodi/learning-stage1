219409e8b167833f57c3d9c51abb733fc95b6335
prateekaryanmodi <prateekaryanmodi@gmail.com>
2026-04-14 21:04:15 +0530

    #WAP to ask the user to enter 3 names of students and store it in a list

---
+++ listing and list slicing.py
@@ -0,0 +1,12 @@
+#WAP to ask the user to enter 3 names of students and store it in a list
+students = []
+roll = input("Enter the name of student 1: ")
+students.append(roll)
+roll = input("Enter the name of student 2: ")
+students.append(roll)
+roll = input("Enter the name of student 3: ")
+students.append(roll)
+print(students)
+
+names= input([])
+print(names)
