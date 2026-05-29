b3582eee97c44e4d12641d5bd2a61c30a1e44c7c
prateekaryanmodi <prateekaryanmodi@gmail.com>
2026-05-29 11:23:08 +0530

    loops

---
+++ code for writing multiplication table.py
@@ -0,0 +1,6 @@
+n= int(input("Enter a number: "))
+m = 1
+while m<=10:
+    a= n*m
+    print(n,"*",m ,"=",a)      
+    m+=1
---
+++ loops.py
@@ -0,0 +1,10 @@
+a= 1
+while a<=100:
+    print(a)
+    a+=1
+
+print("2nd problem")
+b=100
+while b>=1:
+    print(b)
+    b = b-1
