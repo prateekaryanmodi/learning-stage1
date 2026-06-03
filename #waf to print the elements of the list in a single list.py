cd3bc280e56ea3859d752e815311bc4bbc3824e3
prateekaryanmodi <prateekaryanmodi@gmail.com>
2026-06-03 18:15:15 +0530

    Create #waf to print the elements of the list in a single list.py

---
+++ #waf to print the elements of the list in a single list.py
@@ -0,0 +1,6 @@
+#waf to print the elements of the list in single line
+name = ["arjun","sara","prateek","vishesh"]
+def elementlist(list):
+    for item in list:
+        print(item, end=" ")
+elementlist(name)
